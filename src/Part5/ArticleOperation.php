b2e13b78e86070a747bdc58cbb0e4a33419f036b
hyroky867 <handm755@gmail.com>
2023-03-27 14:29:07 +0900

    first commit

---
+++ src/Part5/ArticleOperation.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Part5;
+
+class ArticleOperation
+{
+    private Article $article;
+
+    public function draft(Writer $writer): void
+    {
+    }
+
+    public function subscribe(Subscriber $subscriber): void
+    {
+    }
+}
