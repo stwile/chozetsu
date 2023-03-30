758479e85a8602dd7a9b7f9f175e538b60be46d9
hyroky867 <handm755@gmail.com>
2023-03-30 23:38:11 +0900

    5-20: 具象USBデバイス

---
+++ src/Part5/USBKeyboard.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Part5;
+
+class USBKeyboard
+{
+    public function connect(InternalBus $bus): void
+    {
+    }
+
+    public function typeKey(string $code): void
+    {
+    }
+}
---
+++ src/Part5/USBMouse.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Part5;
+
+class USBMouse
+{
+    public function connect(InternalBus $bus): void
+    {
+    }
+
+    public function moveCursor(float $direction, float $distance): void
+    {
+    }
+}
