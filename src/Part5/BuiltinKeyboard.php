8ada6aa50b5ba22281c1969f17c0e3365c7c8f12
hyroky867 <handm755@gmail.com>
2023-03-30 23:49:24 +0900

    5-24: ビルトインデバイス

---
+++ src/Part5/BuiltinKeyboard.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Part5;
+
+class BuiltinKeyboard
+{
+    public function typeKey(string $code): void
+    {
+    }
+}
---
+++ src/Part5/BuiltinTrackpad.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Part5;
+
+class BuiltinTrackpad
+{
+    public function moveCursor(float $direction, float $distance): void
+    {
+    }
+}
