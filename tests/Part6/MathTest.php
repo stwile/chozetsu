868fce39ca08cc3d57ca35814e14a81c2a90019a
hyroky867 <handm755@gmail.com>
2023-03-31 00:20:58 +0900

    6-5: テストを強化

--- tests/Part6/MathTest.php
+++ tests/Part6/MathTest.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace Tests\Part6;
 
 use App\Part6\Math;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
+
+use const PHP_INT_MAX;
+use const PHP_INT_MIN;
 
 class MathTest extends TestCase
 {
@@ -17,17 +21,33 @@ class MathTest extends TestCase
         $this->model = new Math();
     }
 
-    /** @test */
-    public function minMax(): void
+    #[TestWith([0, 0, 1])]
+    #[TestWith([0, 1, 0])]
+    #[TestWith([-1, 0, -1])]
+    #[TestWith([-1, -1, 0])]
+    #[TestWith([0, 0, 0])]
+    #[TestWith([0, 0, PHP_INT_MAX])]
+    #[TestWith([PHP_INT_MIN, 0, PHP_INT_MIN])]
+    public function testMin(int $expected, int $input_a, int $input_b): void
     {
         $this->assertSame(
-            expected: 1,
-            actual: $this->model->min(1, 2),
+            expected: $expected,
+            actual: $this->model->min($input_a, $input_b),
         );
+    }
 
+    #[TestWith([1, 0, 1])]
+    #[TestWith([1, 1, 0])]
+    #[TestWith([0, 0, -1])]
+    #[TestWith([0, -1, 0])]
+    #[TestWith([0, 0, 0])]
+    #[TestWith([PHP_INT_MAX, 0, PHP_INT_MAX])]
+    #[TestWith([0, 0, PHP_INT_MIN])]
+    public function testMax(int $expected, int $input_a, int $input_b): void
+    {
         $this->assertSame(
-            expected: 2,
-            actual: $this->model->max(1, 2),
+            expected: $expected,
+            actual: $this->model->max($input_a, $input_b),
         );
     }
 }
